bde5ba98f7989d6d71c09c9c2a3d1975d1ea2159
olxia172 <kucharczyk.aleksandra@gmail.com>
2017-12-03 16:55:19 +0100

    Delete related comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy # dzieki relacji, przy usuwaniu artykulu usuwamy tez komentarze
   validates :title, presence: true, length: { minimum: 5 }
 end
