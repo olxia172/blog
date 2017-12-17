1bd2e07b4bbab5778e285bf2c7200f19269d4568
olxia172 <kucharczyk.aleksandra@gmail.com>
2017-12-17 09:48:21 +0100

    Add relations has_many and belogs_to for articles and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :articles, foreign_key: :author_id # wymuszamy zeby korzystalo z kolumny author_id
 end
